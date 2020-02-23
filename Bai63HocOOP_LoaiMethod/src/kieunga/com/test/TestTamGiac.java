9a9ba48fbacf940175271a2cbcc651c8302e4362
KieuNga <KieuNga@kieunga-is>
2020-02-23 16:46:29 +0700

    bai 63

---
+++ Bai63HocOOP_LoaiMethod/src/kieunga/com/test/TestTamGiac.java
@@ -0,0 +1,14 @@
+package kieunga.com.test;
+
+import kieunga.com.model.TamGiac;
+
+public class TestTamGiac {
+
+	public static void main(String[] args) {
+		TamGiac tg1=new TamGiac(20, 12, 25);
+		System.out.println("Chu vi= "+tg1.tinhChuVi());
+		System.out.println("Diện tích= "+tg1.tinhDienTich());
+		
+	}
+
+}
